ad1d7c2d63f5131c11cd159a69fbc844e8bed953
Kirill <nikodakirill386@gmail.com>
2021-08-04 02:28:12 +0300

    initial

---
+++ comments-service/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ content, id: commentId });
+
+  commentsByPostId[req.params.id] = comments;
+});
+
+app.listen(4001, () => {
+  console.log("listening on port 4001");
+});
---
+++ posts-service/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  console.log(req.body);
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+
+  axios.post("http://localhost:4005/events", {
+    type: "post_created",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
